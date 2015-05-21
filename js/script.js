2f1e328523767482c550ad302f8a891a35644cb0
Richard Miles <richlloydmiles@gmail.com>
2015-05-21 19:11:20 +0100

    test

--- js/script.js
+++ js/script.js
@@ -4,15 +4,15 @@ angular.module("helloworld", ['ngSanitize']).controller("HelloController", funct
 	$scope.toggle = function(link) {
 		$scope.cont = link;
 		$scope.load_pages();
+
 	}
 	$scope.load_pages = function(link) {
 		$scope.pages = [];
 		$http.get('http://www.richmiles.co.za/wp-json/pages/').success(function(data) {
 			for (i = 0; i < data.length; i++) { 
-
-				$scope.pages.push(data[i]);
+				$scope.pages.push(data[i]);		
 			}	
-
+			console.log($scope.pages);
 		});
 	};
 });
