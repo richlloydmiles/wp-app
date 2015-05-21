654bbe28e45c8091f4502906a2d01943f5f49ae1
Richard Miles <richlloydmiles@gmail.com>
2015-05-21 17:03:12 +0100

    added init function

--- js/script.js
+++ js/script.js
@@ -1,12 +1,17 @@
 angular.module("helloworld", ['ngSanitize']).controller("HelloController", function($scope, $http ) {
 	$scope.pages = [];
 	$scope.cont = "";
-	$http.get('http://www.richmiles.co.za/wp-json/pages/').success(function(data) {
-		for (i = 0; i < data.length; i++) { 
-			$scope.pages.push(data[i]);
-		}
-	});
 	$scope.toggle = function(link) {
 		$scope.cont = link;
 	}
+	$scope.load_pages = function(link) {
+		$scope.pages = [];
+		$http.get('http://www.richmiles.co.za/wp-json/pages/').success(function(data) {
+			for (i = 0; i < data.length; i++) { 
+
+				$scope.pages.push(data[i]);
+			}	
+		});
+	};
 });
+
